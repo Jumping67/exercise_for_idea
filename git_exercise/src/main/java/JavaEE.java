444f9aec268bf5275f22c9fe77a5ff077d0bc32e
Jumping <470460902@qq.com>
2019-04-24 22:01:03 +0800

    456

--- git_exercise/src/main/java/JavaEE.java
+++ git_exercise/src/main/java/JavaEE.java
@@ -2,5 +2,7 @@ public class JavaEE {
     public static void main(String[] args) {
         System.out.println("Welcome to this wonderful java world!");
         System.out.println("shit,loser!!!!");
+
+        Integer id;
     }
 }
